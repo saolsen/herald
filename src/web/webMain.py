f30622afd380103ac7d6766887f6c88f4d79032d
Stephen Olsen <steveo@hack-net.org>
2011-12-05 16:20:14 -0600

    went back to flask

---
+++ src/web/webMain.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+    Herald
+    ~~~~~~~~
+
+    This is the main web process for the herald API
+
+    :author: Stephen Olsen
+    :copyright: (c) 2011 Herald
+"""
+#flask stuff
+from flask import Flask, request, session, url_for, redirect, \
+    render_template, abort, g, flash
+import mongoengine
+#celery workers
+import sys
+sys.path.append("../")
+import workers.fest.workers as fest
+#import subprocess
+#from boto.s3.connection import S3Connection
+#from boto.s3.key import Key
+
+# configuration
+# pymongo connection is threadsafe, we define it globally
+# We will have to redo this when we have a production environment up
+mongoengine.connect('herald-beta', host='dbh46.mongolab.com', port=27467,
+                    username="herald-beta-user", password='HeraldHacknetBeta')
+
+# create our little application :)
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def serve():
+    """
+    Hey look it works!
+    """
+    return "So that part is good."
+
+@app.route('/tts', methods=['POST'])
+def testit():
+    """
+    Take some text, turn it into speech, same as the fest server
+    """
+    text = request.form['text']
+    name = request.form['name']
+
+    t = fest.tts.delay(text, name)
+    return t.get()
+
+if __name__ == '__main__':
+    # Make sure that festival is running
+    app.run(host="0.0.0.0")
--- src/workers/fest/workers.py
+++ src/workers/fest/workers.py
@@ -11,8 +11,43 @@
 """
 
 from celery.task import task
+import subprocess
+from mongoengine import *
+
+connect('herald-beta', host='dbh46.mongolab.com', port=27467,
+        username="herald-beta-user", password='HeraldHacknetBeta')
 
 # Hello World test task
 @task()
 def testRunning():
     return "Hello World"
+
+# Task that does what my fest server did
+@task()
+def tts(text, name):
+    filename = render_text(text, name)
+
+    #saved_file = WavFile(name)
+    #local_file = open(filename, 'r')
+    #saved_file.wav = local_file
+    #saved_file.save()
+    #local_file.close()
+
+    return filename
+
+# helpers
+def render_text(text, name):
+    """
+    Takes the text, runs in through festival, saves it as _id.wav
+    returns the filename
+    """
+    # I'm thinking that there's a better way to do this without saving the wav file
+    # locally first
+    subprocess.call('echo "' + text + '" | festival_client --ttw | cat > '
+                    + name + '.wav', shell=True)
+    return name + ".wav"
+
+# mongo model
+class WavFile(Document):
+    name = StringField()
+    wav = FileField()
